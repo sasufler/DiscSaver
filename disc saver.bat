9aa5375ecc110459c618659f062d235de2be16c6
Nathan L <haclomteam@gmail.com>
2021-08-28 21:30:54 +0200

    Add files via upload

---
+++ disc saver.bat
@@ -0,0 +1,81 @@
+@echo off
+color a
+chcp 65001
+cls
+title Disc Saver
+echo ██████╗ ██╗███████╗ ██████╗    ███████╗ █████╗ ██╗   ██╗███████╗██████╗ 
+echo ██╔══██╗██║██╔════╝██╔════╝    ██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+echo ██║  ██║██║███████╗██║         ███████╗███████║██║   ██║█████╗  ██████╔╝
+echo ██║  ██║██║╚════██║██║         ╚════██║██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+echo ██████╔╝██║███████║╚██████╗    ███████║██║  ██║ ╚████╔╝ ███████╗██║  ██║
+echo ╚═════╝ ╚═╝╚══════╝ ╚═════╝    ╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+echo.
+echo.
+echo.
+echo Bonjour, bienvenue sur Disc Saver, se script vous permer de fichier les fichier temporaire de vos disques.
+echo Disc Saver peut supprimer le ficher temp prefetch etc.
+echo C'est la version Windows 10 de Disc Saver
+
+pause
+cls
+
+:retour
+echo Appuyez sur 1 pour allez sur les choix de fichier Windows.
+echo Appuyez sur 2 pour allez sur les choix de fichier de Navigateur.
+echo Appuyez sur 3 pour allez sur les choix de fichier de Discord.
+
+
+
+set /p choix=
+if %choix%==1 goto windows
+if %choix%==2 goto Navigateur
+if %choix%==3 goto compress
+
+:windows
+cls
+echo ██████╗ ██╗███████╗ ██████╗    ███████╗ █████╗ ██╗   ██╗███████╗██████╗ 
+echo ██╔══██╗██║██╔════╝██╔════╝    ██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+echo ██║  ██║██║███████╗██║         ███████╗███████║██║   ██║█████╗  ██████╔╝
+echo ██║  ██║██║╚════██║██║         ╚════██║██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+echo ██████╔╝██║███████║╚██████╗    ███████║██║  ██║ ╚████╔╝ ███████╗██║  ██║
+echo ╚═════╝ ╚═╝╚══════╝ ╚═════╝    ╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+echo.
+echo.
+echo.
+start windows.bat
+exit
+
+:Navigateur
+cls
+echo ██████╗ ██╗███████╗ ██████╗    ███████╗ █████╗ ██╗   ██╗███████╗██████╗ 
+echo ██╔══██╗██║██╔════╝██╔════╝    ██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+echo ██║  ██║██║███████╗██║         ███████╗███████║██║   ██║█████╗  ██████╔╝
+echo ██║  ██║██║╚════██║██║         ╚════██║██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+echo ██████╔╝██║███████║╚██████╗    ███████║██║  ██║ ╚████╔╝ ███████╗██║  ██║
+echo ╚═════╝ ╚═╝╚══════╝ ╚═════╝    ╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+echo.
+echo.
+echo.
+start navigateur.bat
+pause
+goto fin
+
+:compress
+cls
+echo ██████╗ ██╗███████╗ ██████╗    ███████╗ █████╗ ██╗   ██╗███████╗██████╗ 
+echo ██╔══██╗██║██╔════╝██╔════╝    ██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+echo ██║  ██║██║███████╗██║         ███████╗███████║██║   ██║█████╗  ██████╔╝
+echo ██║  ██║██║╚════██║██║         ╚════██║██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+echo ██████╔╝██║███████║╚██████╗    ███████║██║  ██║ ╚████╔╝ ███████╗██║  ██║
+echo ╚═════╝ ╚═╝╚══════╝ ╚═════╝    ╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+echo.
+echo.
+echo.
+echo compress marche !
+pause
+pause
+goto fin
+
+:fin
+echo tout est ok !
+pause > nul
---
+++ windows.bat
@@ -0,0 +1,78 @@
+@echo off
+chcp 65001
+cls
+title Disc Saver : Temp Windows
+color a
+
+
+:start
+cls
+echo ██████╗ ██╗███████╗ ██████╗    ███████╗ █████╗ ██╗   ██╗███████╗██████╗ 
+echo ██╔══██╗██║██╔════╝██╔════╝    ██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+echo ██║  ██║██║███████╗██║         ███████╗███████║██║   ██║█████╗  ██████╔╝
+echo ██║  ██║██║╚════██║██║         ╚════██║██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+echo ██████╔╝██║███████║╚██████╗    ███████║██║  ██║ ╚████╔╝ ███████╗██║  ██║
+echo ╚═════╝ ╚═╝╚══════╝ ╚═════╝    ╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+echo.
+echo.
+echo.
+echo Choix 1 : Supprmier les fichier dans le fichier temp de windows. (C:\Users\Utulisateur\AppData\Local\Temp)
+echo Choix 2 : Supprimer les fichier dans le dossier PreFetch de windows (C:\Windows\Prefetch)
+echo Choix 3 : Quitter le logiciel.
+
+
+set /p choix=
+if %choix%==1 goto temp
+if %choix%==2 goto prefetch
+if %choix%==3 goto fin
+
+:temp
+cls
+echo ██████╗ ██╗███████╗ ██████╗    ███████╗ █████╗ ██╗   ██╗███████╗██████╗ 
+echo ██╔══██╗██║██╔════╝██╔════╝    ██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+echo ██║  ██║██║███████╗██║         ███████╗███████║██║   ██║█████╗  ██████╔╝
+echo ██║  ██║██║╚════██║██║         ╚════██║██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+echo ██████╔╝██║███████║╚██████╗    ███████║██║  ██║ ╚████╔╝ ███████╗██║  ██║
+echo ╚═════╝ ╚═╝╚══════╝ ╚═════╝    ╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+echo.
+echo.
+echo.
+echo Voulez vous vraiment supprimer les fichier dans le dossier temp ?
+echo Si Oui Tappez : Y
+echo Si Non Tappez : N
+cd %temp%
+set /p choix=
+if %choix%==Y goto :temp2
+if %choix%==N del goto start
+goto start
+
+:temp2
+cd %temp%
+del *
+goto :start
+
+:prefetch
+cls
+echo ██████╗ ██╗███████╗ ██████╗    ███████╗ █████╗ ██╗   ██╗███████╗██████╗ 
+echo ██╔══██╗██║██╔════╝██╔════╝    ██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+echo ██║  ██║██║███████╗██║         ███████╗███████║██║   ██║█████╗  ██████╔╝
+echo ██║  ██║██║╚════██║██║         ╚════██║██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+echo ██████╔╝██║███████║╚██████╗    ███████║██║  ██║ ╚████╔╝ ███████╗██║  ██║
+echo ╚═════╝ ╚═╝╚══════╝ ╚═════╝    ╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+echo.
+echo.
+echo.
+echo Voulez vous vraiment supprimer les fichier dans le dossier Pretech ?
+echo Si Oui Tappez : Y
+echo Si Non Tappez : N
+cd C:\Windows\Prefetch
+set /p choix=
+if %choix%==Y del *
+if %choix%==N goto start
+
+goto start
+:fin
+exit
+
+
+pause
